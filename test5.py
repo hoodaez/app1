79aeed086ebd51851262ce011448400b0cb139d7
mahmoud ezz <hoodaezo3@hotmail.com>
2024-12-05 01:36:54 +0200

    Add files via upload

--- test5.py
+++ test5.py
@@ -1,8 +1,9 @@
+from math import e
+
 
 number= int(input (" enter your number"))
 
 if number > 0 :
-
     print (" your numbre is pstaive")
 elif number < 0:
     print (" your number is bostaive")
